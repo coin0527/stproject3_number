120f0abd9a53054b002e72259782a9b792c2137c
coin0527 <nako2020@naver.com>
2024-01-17 17:21:11 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { useState } from "react";
 
 const Wrap = styled.div``;
 const Container = styled.div`
-  border: 1px solid black;
   width: 100%;
   height: 80vh;
   padding: 30px;
@@ -25,37 +25,47 @@ const Circle = styled.div`
   height: 300px;
   margin-bottom: 30px;
   margin-top: 30px;
+  font-size: 75px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const Button = styled.button`
-  ${({ theme }) => css`
-    background-color: ${theme.primaryColor};
-    color: ${theme.textColor};
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    transition: background-color 0.3s;
+  background-color: #333;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: 0.3s;
 
-    &:hover {
-      background-color: ${theme.primaryHoverColor};
-    }
-  `}
+  &:hover {
+    background-color: gray;
+  }
 `;
-const theme = {
-  primaryColor: "#333",
-  textColor: "white",
-  primaryHoverColor: "gray",
-};
+const Range = styled.div`
+  margin-bottom: 30px;
+`;
 
 export const App = () => {
+  const [randomNum, setRandomNum] = useState(null);
+
+  const RandomNumber = () => {
+    const randomValue = Math.floor(Math.random() * 100) + 1;
+    setRandomNum(randomValue);
+  };
   return (
     <Wrap>
       <Header />
       <Container>
         <Title> Select your Number </Title>
-        <Circle></Circle>
-        <Button theme={theme}>생성하기</Button>
+        <Circle>{randomNum}</Circle>
+        <Range>
+          <h3>범위</h3>
+          {/* 인풋 2개 */}
+        </Range>
+        <Button onClick={RandomNumber}>생성하기</Button>
       </Container>
       <Footer />
     </Wrap>
