867577fca7720f67c4087adf5a9f05d8341e60b8
coin0527 <nako2020@naver.com>
2024-01-19 22:45:35 +0900

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,12 @@ const Wrap = styled.div`
   width: 100%;
   height: 60px;
   background-color: beige;
+  text-align: center;
+  position: absolute;
+  bottom: 0;
+  line-height: 60px;
 `;
 
 export const Footer = () => {
-  return <Wrap></Wrap>;
+  return <Wrap>&copy; eden27</Wrap>;
 };
